f1b66bddfb66d36f06335029099d28a26f892c76
Marco Lussetti <marco@marcolussetti.com>
2020-03-07 01:44:06 -0800

    Fix fix fix

--- icmpchecker.go
+++ icmpchecker.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net"
 	"time"
-	"errors"
 	"github.com/tatsushid/go-fastping"
 )
 
@@ -90,7 +89,7 @@ func (c ICMPChecker) doChecks() Attempts {
 		}
 		if !didRespond {
 			checks[i].RTT = time.Since(start)
-			checks[i].Error = errors.New("Did not answer pings.")
+			checks[i].Error = fmt.Errorf("Did not answer pings.")
 			continue
 		}
 	}
